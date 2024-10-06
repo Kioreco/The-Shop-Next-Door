360a5b6a71f564101ee63157a0aeac16498420c3
PabloPrior <pabloprior2107@gmail.com>
2024-10-06 12:53:38 +0200

    Instanciación en el GameManager y posiciones diferentes

--- UNITY/The Shop Next Door/Assets/_Scripts/Managers/GameManager.cs
+++ UNITY/The Shop Next Door/Assets/_Scripts/Managers/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -9,6 +10,14 @@ public class GameManager : MonoBehaviour
     private NetworkManager _networkManager;
 
     private GameObject _playerPrefab;
+
+    private List<Vector3> _spawnPositions = new List<Vector3>()
+    {
+        new Vector3(1, 0, 3),
+        new Vector3(-15, 0, -20),
+    };
+
+    private int _spawnIndex = 0;
 
     void Start()
     {
@@ -21,9 +30,13 @@ public class GameManager : MonoBehaviour
 
     private void OnClientConnected(ulong obj)
     {
+        if (NetworkManager.Singleton.IsServer) 
+        {
+            var player = Instantiate(_playerPrefab, _spawnPositions[_spawnIndex], _playerPrefab.transform.rotation); 
+            player.GetComponent<NetworkObject>().SpawnAsPlayerObject(obj); 
 
-        var player = Instantiate(_playerPrefab);
-        player.GetComponent<NetworkObject>().SpawnAsPlayerObject(obj);
+            _spawnIndex++;
+        }
     }
 
     private void OnServerStarted()
