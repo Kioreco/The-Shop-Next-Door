96c0a390b5a11f120e34a740be2044063ea06470
Pablo <p.garcian.2021@alumnos.urjc.es>
2024-10-09 00:35:06 +0200

    Title Screen prototipada

--- UNITY/The Shop Next Door/Assets/_Scripts/Managers/UIManager.cs
+++ UNITY/The Shop Next Door/Assets/_Scripts/Managers/UIManager.cs
@@ -1,85 +1,39 @@
-using Unity.Netcode;
-using Unity.Netcode.Transports.UTP;
-using Unity.Networking.Transport.Relay;
-using Unity.Services.Authentication;
-using Unity.Services.Core;
-using Unity.Services.Relay;
-using Unity.Services.Relay.Models;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    private const int maxConnections = 50;
-    private string joinCode = "Room code";
-    void OnGUI()
+    [Header("TITLE SCENE")]
+    [SerializeField] private GameObject TSND_Logo;
+
+    [SerializeField] private GameObject LoginCreateButtons;
+
+    [SerializeField] private GameObject LoginCreatePanel;
+
+    public static UIManager Instance { get; private set; }
+    void Awake()
     {
-        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        if (Instance == null)
         {
-            StartButtons();
+            Instance = this;
         }
         else
         {
-            StatusLabels();
-        }
-
-        GUILayout.EndArea();
-    }
-
-    void StartButtons()
-    {
-        if (GUILayout.Button("Host")) StartHost();
-        if (GUILayout.Button("Client")) StartClient();
-        joinCode = GUILayout.TextField(joinCode);
-    }
-
-    async void StartHost()
-    {
-        await UnityServices.InitializeAsync();
-
-        if (!AuthenticationService.Instance.IsSignedIn)
-        {
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
-        }
-
-        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
-        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "wss"));
-        joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-
-        NetworkManager.Singleton.StartHost();
-    }
-
-    async void StartClient()
-    {
-        await UnityServices.InitializeAsync();
-        if (!AuthenticationService.Instance.IsSignedIn)
-        {
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
-        }
-
-        var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
-        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "wss"));
-
-        NetworkManager.Singleton.StartClient();
-    }
-
-    void StatusLabels()
-    {
-        var mode = NetworkManager.Singleton.IsHost ?
-            "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
-
-        GUILayout.Label("Transport: " +
-            NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
-        GUILayout.Label("Mode: " + mode);
-
-        GUILayout.Label("Code: " + joinCode);
-
-        if (GUILayout.Button("Copiar codigo"))
-        {
-            TextEditor textEditor = new TextEditor();
-            textEditor.text = joinCode;
-            textEditor.SelectAll();
-            textEditor.Copy();
+            Destroy(gameObject);
         }
     }
+
+    public void CloseTitleScreen()
+    {
+        TSND_Logo.SetActive(false);
+        LoginCreateButtons.SetActive(true);
+    }
+
+    public void OpenLoginCampus()
+    {
+        LoginCreateButtons.SetActive(false);
+        LoginCreatePanel.SetActive(true);
+    }
+
 }
---
+++ UNITY/The Shop Next Door/Assets/_Scripts/Managers/UIManager_Relay.cs
@@ -0,0 +1,85 @@
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using UnityEngine;
+
+public class UIManager_Relay : MonoBehaviour
+{
+    private const int maxConnections = 50;
+    private string joinCode = "Room code";
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            StartButtons();
+        }
+        else
+        {
+            StatusLabels();
+        }
+
+        GUILayout.EndArea();
+    }
+
+    void StartButtons()
+    {
+        if (GUILayout.Button("Host")) StartHost();
+        if (GUILayout.Button("Client")) StartClient();
+        joinCode = GUILayout.TextField(joinCode);
+    }
+
+    async void StartHost()
+    {
+        await UnityServices.InitializeAsync();
+
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "wss"));
+        joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+        NetworkManager.Singleton.StartHost();
+    }
+
+    async void StartClient()
+    {
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+
+        var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "wss"));
+
+        NetworkManager.Singleton.StartClient();
+    }
+
+    void StatusLabels()
+    {
+        var mode = NetworkManager.Singleton.IsHost ?
+            "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+
+        GUILayout.Label("Transport: " +
+            NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+        GUILayout.Label("Mode: " + mode);
+
+        GUILayout.Label("Code: " + joinCode);
+
+        if (GUILayout.Button("Copiar codigo"))
+        {
+            TextEditor textEditor = new TextEditor();
+            textEditor.text = joinCode;
+            textEditor.SelectAll();
+            textEditor.Copy();
+        }
+    }
+}
